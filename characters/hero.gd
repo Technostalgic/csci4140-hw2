1187517b8f89470657c169bc3ea8261bf9bb1fc7
technostalgic <tteck02@gmail.com>
2026-09-05 15:20:33 -0600

    implement hero animations

--- characters/hero.gd
+++ characters/hero.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@export var happy_boo: Node2D = null
 @export var movement_speed: float = 600
 
 const SPEED = 300.0
@@ -7,14 +8,18 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	
 	var movement = Input.get_vector(
 		"move_left", 
 		"move_right", 
 		"move_up", 
 		"move_down"
 	)
-	
 	velocity = movement * movement_speed
-	
+	handle_animation()
 	move_and_slide()
+
+func handle_animation():
+	if velocity.length() > 1:
+		happy_boo.play_walk_animation()
+	else: 
+		happy_boo.play_idle_animation()
