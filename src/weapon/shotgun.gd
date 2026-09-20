1d06a00a36a13c4aa400de753c652c77c97941bf
Your Name <you@example.com>
2026-09-19 18:31:47 -0600

    add weapon trails

--- src/weapon/shotgun.gd
+++ src/weapon/shotgun.gd
@@ -2,6 +2,7 @@ class_name Shotgun
 extends Gun
 
 @export var shoot_sound: AudioStream = null
+@export var impact_effect: PackedScene = null
 @export var audio_player: AudioStreamPlayer = null
 @export var raycast: RayCast2D = null
 @export var bullets_per_shot: int = 6
@@ -9,19 +10,55 @@ extends Gun
 @export_range(0, 1) var evenly_spread_factor: float = 0.8
 @export var weapon_range: float = 1000
 @export var damage: float = 1
-@export var environmental_impact_effect: PackedScene = null
+@export var trail_fade_rate: float = 20
+
+var bullet_trail_pool: Array[Line2D] = []
+var _cur_trail_index: int = 0
 
 func _ready() -> void:
 	super._ready()
 	audio_player.stream = shoot_sound
+	_pool_trails()
+
+func _process(delta: float) -> void:
+	_fade_trails(delta)
+	super._process(delta)
+
+func _pool_trails() -> void:
+	_cur_trail_index = 0
+	for i in range(10):
+		var trail = Line2D.new()
+		trail.width = 3
+		trail.default_color = Color("gold")
+		trail.points = PackedVector2Array([
+			Vector2.ZERO,
+			Vector2(1, 0),
+		])
+		get_tree().root.add_child.call_deferred(trail)
+		bullet_trail_pool.push_back(trail)
+		trail.modulate.a = 0
+
+func _fade_trails(delta: float) -> void:
+	for trail in bullet_trail_pool:
+		trail.modulate.a -= delta * trail_fade_rate
+
+func get_trail() -> Line2D:
+	_cur_trail_index += 1
+	if _cur_trail_index >= bullet_trail_pool.size():
+		_cur_trail_index = 0
+	return bullet_trail_pool[_cur_trail_index]
 
 func _fire_shot(direction: float):
 	var endpoint = Vector2.from_angle(direction - projectile_spawnpoint.global_rotation) * weapon_range
+	if projectile_spawnpoint.global_scale.y < 0:
+		endpoint.y *= -1
 	raycast.target_position = endpoint
 	raycast.force_raycast_update()
 	
+	var trail_length := weapon_range
 	if raycast.is_colliding():
 		var hit_obj := raycast.get_collider()
+		var hit_point :=  raycast.get_collision_point()
 		if hit_obj is Mob:
 			var mob := hit_obj as Mob
 			if mob.health <= 0:
@@ -30,9 +67,19 @@ func _fire_shot(direction: float):
 				return
 			mob.take_damage(damage)
 		
-		var impact := environmental_impact_effect.instantiate() as Node2D
+		trail_length = hit_point.distance_to(raycast.global_position)
+		
+		# create impact effect
+		var impact := impact_effect.instantiate() as Node2D
 		get_tree().root.add_child(impact)
-		impact.global_position = raycast.get_collision_point()
+		impact.global_position = hit_point
+	
+	# create bullet trail
+	var trail = get_trail()
+	trail.global_position = raycast.global_position
+	trail.global_rotation = direction
+	trail.scale.x = trail_length
+	trail.modulate.a = 1
 
 func fire():
 	audio_player.play()
